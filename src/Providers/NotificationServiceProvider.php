a35f78f18818b2da6d1f4a24dbe69c602e63de8d
zaxxas <sakiyamajunjo02@gmail.com>
2023-02-23 22:13:42 +0900

    fix: logic bugs

--- src/Providers/NotificationServiceProvider.php
+++ src/Providers/NotificationServiceProvider.php
@@ -13,7 +13,7 @@ class NotificationServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '../config/notification.php' => config_path('notification.php'),
+            __DIR__ . '/../config/notification.php' => config_path('notification.php'),
         ]);
     }
     /**

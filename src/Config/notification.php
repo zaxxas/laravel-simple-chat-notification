962ba6b18e61d6a3720790a91e01aa2c439a72da
zaxxas <sakiyamajunjo02@gmail.com>
2023-02-27 19:36:59 +0900

    fix: config directory

---
+++ src/Config/notification.php
@@ -0,0 +1,19 @@
+<?php
+
+use Zaxxas\NotifyToChatTools\Enums\NotificationTool;
+
+return [
+    'tool' => env('NOTIFICATION_TOOL', NotificationTool::Slack->value),
+    'teams' => [
+        'webhook_url' => env('TEAMS_WEBHOOK_URL'),
+    ],
+    'slack' => [
+        'webhook_url' => env('SLACK_WEBHOOK_URL'),
+        'channel'     => env('SLACK_NOTIFICATION_CHANNEL'),
+        'sender_name' => env('SLACK_NOTIFICATION_SENDAR_NAME'),
+    ],
+    'line' => [
+        'endpoint_url' => env('LINE_API_ENDPOINT'),
+        'token' => env('LINE_API_TOKEN'),
+    ]
+];
